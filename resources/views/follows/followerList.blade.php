82971e94be4c24dd2f7343e37908854306030baf
teruteru33 <ruaki3kateto30@gmail.com>
2023-11-21 08:43:33 +0900

    フォロー、フォロワーの画像リンクの書き換え

--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -2,14 +2,14 @@
 @section('content')
 
 @foreach ($follows as $follow)
-  <img src="/images/{{ $follow->images }}">
+  <img src="storage/images/{{ $follow->images }}">
 @endforeach
 
 
 @foreach($posts as $posts)
 
 <div>
-  <img src="/images/{{ $posts->images }}">
+  <img src="storage/images/{{ $posts->images }}">
 {{ $posts-> username}}
 {{ $posts->updated_at }}
 </div>
